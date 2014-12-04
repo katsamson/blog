7cce92f339b3e975768883bbc0a50a36e8e00aa0
katsamson <katsamson@users.noreply.github.com>
2014-12-04 09:23:18 -0600

    delcared HOLD

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 "use strict"; // This line chooses a JavaScript dialect, one that helps both jsLint (used in OrionHub) and browsers catch errors.
 /*jslint browser: true*/ // This line tells jsLint that the code will run in a browser.
 
+var hold;
+
 function Random_Post() {
 var random_answer;
 random_answer = Math.random() * options.length;
