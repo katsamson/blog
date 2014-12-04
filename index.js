f15ea17e8233676559773d843436fe8eb7f69310
aschulte1919 <aschulte@nebrwesleyan.edu>
2014-12-03 22:22:11 -0500

    wow mom lol

--- index.js
+++ index.js
@@ -3,15 +3,6 @@
 
 var options = ['http://www.thedailydogblog.com/', 'http://www.alfiesblog.com/entlebucher-mountain-dogs-tips-training-humans/top-dog-blogs/', 'http://ohmydogblog.com/', 'http://campcookstown.com/dog-blogs/', 'http://herdogblog.com/', 'http://www.dogstardaily.com/blogs', 'http://www.lifewithdogs.tv/category/blog/','http://nodogaboutit.wordpress.com/','http://www.allthingsdogblog.com/']
 
-//function random_post();
-  //  document.getElementById('response').innerHTML = random_post();
-	//var random_move;
-    //random_move = Math.random() * options.length;
-    //random_move = Math.floor(random_move);
-    //random_move = options[random_move];
-    //return random_move;
-
-//}
 function random_post() {
 	
     var random_move
